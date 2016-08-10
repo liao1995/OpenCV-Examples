64d1d119d3768142e33adc2e25ea423f8f9a3e79
liao1995 <346950981@qq.com>
2016-08-10 20:37:55 +0800

    Create Image Header: Image Addition in ROI

--- BasicDataStructure/ROI.cpp
+++ BasicDataStructure/ROI.cpp
@@ -8,6 +8,8 @@
 #include "cv.h"
 #include "highgui.h"
 
+const char* filename = "corji.jpg";
+
 void setROI(IplImage* src, CvRect rect, int add)
 {
 	cvSetImageROI( src, rect );
@@ -15,9 +17,31 @@ void setROI(IplImage* src, CvRect rect, int add)
 	cvResetImageROI( src );
 }
 
+void ROIImage_demo()
+{
+	IplImage* image = cvLoadImage( filename );
+	assert( image );
+	CvRect roi_rect = cvRect(115, 40, 300, 255);
+	IplImage* roi_img = cvCreateImageHeader (
+		cvSize(roi_rect.width, roi_rect.height)	,
+		image->depth,
+		image->nChannels
+		);
+	roi_img->widthStep = image->widthStep;
+	roi_img->origin = image->origin;
+	roi_img->imageData = image->imageData +
+			roi_rect.y * image->widthStep +
+			roi_rect.x * image->nChannels;
+	cvNamedWindow( "ROI_SUB" );
+	cvAddS( roi_img, cvScalar(100), roi_img );
+	cvReleaseImageHeader( &roi_img );
+	cvShowImage( "ROI_SUB", image );
+	cvReleaseImage( &image );
+	cvWaitKey( 0 );
+	cvDestroyWindow( "ROI_SUB" );
+}
 void ROI_demo()
 {
-	const char* filename = "corji.jpg";
 	IplImage* image = cvLoadImage( filename );
 	assert( image );
 	setROI( image, cvRect(115, 40, 300, 255), 100 );
@@ -30,6 +54,7 @@ void ROI_demo()
 
 int main( int argc, char* argv[] )
 {
-	ROI_demo();
+//	ROI_demo();
+	ROIImage_demo();
 	return 0;
 }
